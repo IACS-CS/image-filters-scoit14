1f719fe0b3e65b7d9207fbd206f3c0df59462ae8
scoit14 <146465521+scoit14@users.noreply.github.com>
2024-01-24 14:21:58 +0000

    Started and almost completed shape filter.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { brighten } from './filters/brighten'
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+const filters : Filter[] = [
+brightenFilter, 
+roseColoredGlasses, 
+grid, 
+vignette,
+];
+
+export default filters;
